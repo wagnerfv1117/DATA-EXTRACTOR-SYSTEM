4ae040399fc23f26641b9ff3e70a566a423d4c27
Wagner Fernandez <wagnerfernandez17@gmail.com>
2023-08-24 17:14:01 -0500

    code settings

--- code/EXTRACTOR_GUI.py
+++ code/EXTRACTOR_GUI.py
@@ -4,17 +4,17 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import pytesseract
 
-# 2-Configura la ubicación de Tesseract OCR (asegúrate de que esté instalado en tu sistema)
+# 2-Se procede con configurar la ubicación de Tesseract OCR (asegúrate de que esté instalado en tu sistema)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# Variables globales para almacenar la imagen y el texto extraído
+#3-Se definen las variables globales para almacenar la imagen y el texto extraído
 image_path = None
 extracted_text = ""
 
-# 3-Se crea un función para cargar la imagen y mostrarla en la interfaz gráfica
+# 4-Se crea una función para cargar la imagen y mostrarla en la interfaz gráfica - (para saber que es lo que se está mostrando)
 def load_image():
     global image_path
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])#formatos admitidos para extraer imágenes que contengan texto
     if file_path:
         image_path = file_path
         image = Image.open(file_path)
@@ -23,7 +23,7 @@ def load_image():
         image_label.config(image=img)
         image_label.image = img
 
-# 4-Se diseña una función para extraer el texto de la imagen cargada en el programa
+# 5-Se diseña una función para extraer el texto de la imagen cargada en el programa
 def extract_text():
     global extracted_text
     if image_path:
@@ -33,7 +33,7 @@ def extract_text():
         text_box.insert(tk.END, extracted_text)
 
 
-# 5-Se elaborauna función para exportar el texto a un archivo .txt o texto plano
+# 6-Se elabora una función para exportar el texto a un archivo .txt o texto plano
 def export_text():
     if extracted_text:
         file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
@@ -41,7 +41,7 @@ def export_text():
             with open(file_path, "w") as file:
                 file.write(extracted_text)
 
-# 6-Se documenta una función para borrar la imagen cargada y restablecer el cuadro de texto
+# 7-Se documenta una función para borrar la imagen cargada y restablecer el cuadro de texto
 def clear_image():
     global image_path, extracted_text
     image_path = None
@@ -49,11 +49,11 @@ def clear_image():
     image_label.config(image="")
     text_box.delete("1.0", tk.END)
 
-# 7-Parámetros para establecer la configuración de la interfaz gráfica
+# 8-Se estipulan parámetros para establecer la configuración de la interfaz gráfica
 root = tk.Tk()
 root.title("Data Extractor System - DES")
 
-# 7.1 - Nombre del diseñador o desarrollador
+# 8.1 - Se desarrolla una función donde muestre el nombre del diseñador o desarrollador del programa
 designer_name = "Wagner Fernandez V. - (c) 2023 v0.1"  
 
 designer_label = tk.Label(root, text=f"Designed by {designer_name}")
